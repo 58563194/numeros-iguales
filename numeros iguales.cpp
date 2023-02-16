cdf95d585e1b6a91d9612a6c0b64557951c7dd9e
58563194 <124597576+58563194@users.noreply.github.com>
2023-02-16 17:01:58 -0600

    Add files via upload

---
+++ numeros iguales.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	
+	char arr1[5];
+    char arr2[5];
+    int arr1, arr2;
+	
+	for(int i=0; i<3; i++){
+		cout<<"INGRESE UNA FRASE DE 4 ESPACIOS: ";
+		
+		cout<<"["<<i+1<<"]";
+		
+		cin>>arr1[i];
+		
+		a = strlen(a[i]);
+	}
+		
+		
+		for(int j=0; j<3; j++){
+			cout<<"INGRESE OTRA FRASE DE 4 DIGITOS: ";
+			
+			cout<<"["<<j+1<<"]";
+		cin>>arr2[j];
+			
+			
+		}
+		
+		char x=arr1[0], z=arr2[0];
+		cout <<x<<"y"<<z<<endl;
+			if(x==z){
+				
+				cout<<"LAS FRASES INGRESADAS TIENEN LA MISMA CANTIDAD DE LETRAS";
+			}
+			
+			else{
+				
+				cout<<"LAS FRASES INGRESADAS NO SON IGUALES";
+		
+	
+	}
+	
+	return 0;
+}
